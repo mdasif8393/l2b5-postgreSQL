5b3d7de2fedcec60f3760c6ba0c7b190b15ccb77
mdasif8393 <md.asif.8393@gmail.com>
2025-05-16 11:40:48 +0600

    create table

--- sql/table.sql
+++ sql/table.sql
@@ -1,7 +1,14 @@
-create table Persons (
-    PersonID INTEGER,
-    FirstName VARCHAR(50),
-    LastName VARCHAR(50),
-    Address VARCHAR(50),
-    City VARCHAR(50)
-)
+---- create a table
+
+-- create table Persons (
+--     PersonID INTEGER,
+--     FirstName VARCHAR(50),
+--     LastName VARCHAR(50),
+--     Address VARCHAR(50),
+--     City VARCHAR(50)
+-- )
+
+---- create a table from an existing table
+create table TestTable as
+select PersonID, FirstName
+from Persons;
